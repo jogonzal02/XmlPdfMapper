4228dc40865b0194a09763e7e8520b2852ee9c9f
John Gonzalez <jogonzal02@gmail.com>
2018-05-25 00:06:36 -0400

    Grab dataset node from Pdf as well as development on the visual component of the program

--- XmlPdfMapper/XmlPdfMapper/Controllers/HomeController.cs
+++ XmlPdfMapper/XmlPdfMapper/Controllers/HomeController.cs
@@ -1,11 +1,13 @@
 ﻿using iTextSharp.text.pdf;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Web;
 using System.Web.Mvc;
 using System.Xml;
+using System.Xml.Linq;
 
 /// <TODO>
 ///     For now
@@ -21,56 +23,55 @@ namespace XmlPdfMapper.Controllers
 {
     public class HomeController : Controller
     {
-        /// <summary>
-        /// As of right now, this method grabs a xfa pdf convert it into a XML document and inserts the content of the XML document into a string builder
-        /// Run the program to see a large XML string in the bottom of the index page.
-        /// </summary>
-        /// <returns></returns>
 
         public ActionResult Index()
         {
-            //The XFA based pdfs (First one is the document Allstate uses)
-            var path = @"C:\Users\jag27\Documents\Personal\Infosys\AllState\Projects\XmlPdfMapper\XmlPdfMapper\Assets\ABJ45A1AL.pdf";
-            //XFA pdf I found on the internet (only here for testing purposes)
-            //var path = @"C:\Users\jag27\Documents\Personal\Infosys\AllState\Projects\XmlPdfMapper\XmlPdfMapper\Assets\purchase_order.pdf";
-            PdfReader reader = new PdfReader(path);
+            //This is jsut a hard coded in path to a XFA based pdf (this should be change to your specific path)
+            var src = @"C:\Users\jag27\Documents\Personal\Infosys\AllState\Projects\XmlPdfMapper2\XmlPdfMapper\XmlPdfMapper\Assets\ABJ45A1AL.pdf";
 
-
-            // grab xfa field and convert to XML document
+            //Reads the PDF
+            PdfReader reader = new PdfReader(src);
+            //Grabs the forms
             XfaForm xfa = new XfaForm(reader);
-            XmlDocument xml = xfa.DomDocument;
+
+            //Grab the dataset noe
+            XmlNode node = xfa.DatasetsNode;
             reader.Close();
 
-            if (!string.IsNullOrEmpty(xml.DocumentElement.NamespaceURI))
-            {
-                xml.DocumentElement.SetAttribute("xmlns", "");
-                XmlDocument new_xml = new XmlDocument();
-                new_xml.LoadXml(xml.OuterXml);
-                xml = new_xml;
-            }
-
+            //Write the dataset node into a string builder and store it into a ViewBag
             var sb = new StringBuilder(4000);
-            var Xsetting = new XmlWriterSettings() { Indent = true };
-            using (var writer = XmlWriter.Create(sb, Xsetting))
+            var Xsettings = new XmlWriterSettings() { Indent = true };
+            XmlDocument doc = new XmlDocument();
+            doc.LoadXml(node.OuterXml);
+            using (var writer = XmlWriter.Create(sb, Xsettings))
             {
-                xml.WriteTo(writer);
+                doc.WriteTo(writer);
             }
             ViewBag.Form = sb.ToString();
+
+
+            //---------------------------------------------------------------------------------------------------------------------------------------------------------
+            //Loads a XML document(hard coded right now) and split them by new line
+            //This is jsut a hard coded in path to a XFA based pdf (this should be change to your specific path)
+            XDocument x = XDocument.Load(@"C:\Users\jag27\Documents\Personal\Infosys\AllState\Projects\XmlPdfMapper2\XmlPdfMapper\XmlPdfMapper\Assets\XmlExample.xml");
+
+            var xmlArr = x.ToString().Split('\n');
+
+            ViewBag.Xml = xmlArr;
+
+
+
             return View();
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
+        //Opens and read a pdf file(hard coded right now)
+        public FileStreamResult GetPdf() {
+            //This is jsut a hard coded in path to a XFA based pdf (this should be change to your specific path)
+            FileStream fs = new FileStream(@"C:\Users\jag27\Documents\Personal\Infosys\AllState\Projects\XmlPdfMapper2\XmlPdfMapper\XmlPdfMapper\Assets\PdfFormExample.pdf", FileMode.Open, FileAccess.Read);
+            return File(fs, "application/pdf");
         }
 
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
 
-            return View();
-        }
+
     }
 }
